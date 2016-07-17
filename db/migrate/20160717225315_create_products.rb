67ebb8508befba70efad4b52edb0a6a891bfb7f8
Anton Paisov <paisov@gmail.com>
2016-07-18 02:04:45 +0300

    Products scaffold. Further project setup

---
+++ db/migrate/20160717225315_create_products.rb
@@ -0,0 +1,11 @@
+class CreateProducts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.integer :price
+      t.integer :stock_quantity
+
+      t.timestamps
+    end
+  end
+end
