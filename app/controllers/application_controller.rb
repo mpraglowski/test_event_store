dddaeadceb3de54d76d3258133f2a8e76d67ff08
Anton Paisov <paisov@gmail.com>
2016-07-18 03:41:12 +0300

    Added current order

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_filter :set_current_order
+
+  protected
+
+  def set_current_order
+    @current_order = Order.last&.in_progress? ? Order.last : Order.create
+  end
 end
