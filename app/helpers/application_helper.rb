a39557a630315c0ca3905409288b896fc62ddf18
Anton Paisov <paisov@gmail.com>
2016-07-18 04:52:51 +0300

    Basic mechanics in place

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def order_product_button(product)
+    if @current_order.products.include?(product)
+      link_to "Remove from cart", order_products_path(product_id: product.id), method: :delete
+    else
+      link_to "Add to cart", order_products_path(product_id: product.id), method: :post
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :order_products
-  resources :orders
   root to: "products#index"
+
+  resources :orders
   resources :products
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resource :order_products, only: [:create, :destroy]
 end
